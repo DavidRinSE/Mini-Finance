6e4d43fa86e200a061bd1b3a1f82bef01fb26e48
DavidRinSE <davidrichardsonIN@gmail.com>
2020-07-04 19:20:38 -0400

    updated graph to recharts

--- src/react/components/data/History.js
+++ src/react/components/data/History.js
@@ -2,7 +2,7 @@ import React, {useEffect} from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import { useDispatch, useSelector } from "react-redux"
 import { getHistory } from "../../../redux"
-import HistoryCharts from "./HistoryCharts"
+import {ResponsiveContainer, LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Line, Legend} from "recharts"
 
 const useStyles = makeStyles({
     wrapper: {
@@ -28,6 +28,15 @@ const History = (props) => {
     const dispatch = useDispatch()
     let history = useSelector(state => state.history.getHistory)
 
+    let data = () => history.result.map(historyObj => (
+        {
+            name: `${historyObj.startDate.slice(5)}`,
+            balance: parseFloat(historyObj.balance / 100).toFixed(2),
+            income: parseFloat(historyObj.income / 100).toFixed(2),
+            expense: parseFloat(historyObj.expense / 100).toFixed(2)
+        }
+    ))
+
     useEffect(() => {
         if(!history.result && !history.loading){
             dispatch(getHistory())
@@ -37,7 +46,19 @@ const History = (props) => {
     return (
         <div className={classes.wrapper}>
             <h1 className={classes.title}>History</h1>
-            {history.result && <HistoryCharts data={history.result}/>}
+            {history.result &&
+                <ResponsiveContainer width="99%" aspect={2}>
+                    <LineChart width="100%" data={data()} margin={{top: 0, right: 0, left: -30, bottom: 0}}>
+                        <XAxis dataKey="name" style={{fontSize:13}}/>
+                        <YAxis style={{fontSize:13}}/>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <Tooltip separator=": $"/>
+                        <Legend verticalAlign="top" height={36}/>
+                        <Line type="monotone" dataKey="income" stroke="#8884d8" dot={{r:3}} />
+                        <Line type="monotone" dataKey="expense" stroke="#82ca9d" dot={{r:3}}/>
+                    </LineChart>
+                </ResponsiveContainer>
+            }
         </div>
     )
 }
