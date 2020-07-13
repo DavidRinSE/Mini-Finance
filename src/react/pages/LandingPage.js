6cc90e31f84b1ca5886ebce49ef26827b67717da
DavidRinSE <davidrichardsonin@gmail.com>
2020-07-13 07:45:57 -0400

    more updates to landing page

--- src/react/pages/LandingPage.js
+++ src/react/pages/LandingPage.js
@@ -7,9 +7,11 @@ const useStyles = makeStyles({
         background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
         display: "flex",
         flexDirection: "column",
-        justifyContent: "space-around",
+        justifyContent: "space-between",
         alignItems: "center",
-        height: "100vh",
+        height: "94vh",
+        paddingTop:"3vh",
+        paddingBottom:"3vh"
     },
         titleWrap: {
             display: "flex",
@@ -24,18 +26,35 @@ const useStyles = makeStyles({
             },
             subtitle: {
                 color: "white",
-                fontSize:18,
+                fontSize:16,
                 fontFamily:'FredokaOne'
             },
             link: {
                 color: "white",
                 textDecoration: "underline",
                 fontFamily:'FredokaOne',
-                marginTop:3
+                marginTop:5
             },
         picture: {
             height: '55vh',
-        }
+        },
+        headerContent: {
+            backgroundColor: "white",
+            width: "85vw",
+            height: "100px",
+            boxShadow: "3px 3px 4px rgba(0, 0, 0, .5)",
+            borderRadius: 3,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+            alignItems: "center"
+        },
+            headerContentTitle: {
+                fontFamily:'FredokaOne',
+            },
+            unorderedList: {
+                listStyleType: "circle",
+            }
 })
 
 const LandingPage = (props) => {
@@ -49,7 +68,12 @@ const LandingPage = (props) => {
             </div>
             <img className={classes.picture} src={require('../../pictures/App-Picture.png')} alt="Screenshot of the application"/>
             <div className={classes.headerContent}>
-                
+                <h3 className={classes.headerContentTitle}>A portfolio project built with:</h3>
+                <ul className={classes.unorderedList}>
+                    <li>Mobile first design</li>
+                    <li>Latest frameworks and tools</li>
+                    <li>GraphQL API backend</li>
+                </ul>
             </div>
         </div>
     )
