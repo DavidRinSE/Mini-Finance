79453d45bee6f091984bbab8eabab2dbb7ff01ed
DavidRinSE <DavidRichardsonIN@gmail.com>
2020-07-15 12:02:29 -0400

    travis-ci deployment to s3

---
+++ build/precache-manifest.31d598c86a0e30e173c834b973661480.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836e327b1886b376a11ddbc1461e826d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf09baf322e22b3791c",
+    "url": "/static/css/main.43020972.chunk.css"
+  },
+  {
+    "revision": "563cdf1762c99a265d36",
+    "url": "/static/js/2.33026a11.chunk.js"
+  },
+  {
+    "revision": "d82b82ba8e0ff500ee3d4332abcbbe9a",
+    "url": "/static/js/2.33026a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf09baf322e22b3791c",
+    "url": "/static/js/main.1d39a669.chunk.js"
+  },
+  {
+    "revision": "b78427c667928d7677d2",
+    "url": "/static/js/runtime-main.e19d0c2d.js"
+  },
+  {
+    "revision": "9e4e30780c9e6f6a9405f097f9151dc1",
+    "url": "/static/media/App-Picture.9e4e3078.png"
+  },
+  {
+    "revision": "4a2f2ea45a0bb1abe81b47d0afde4aae",
+    "url": "/static/media/FredokaOne-Regular.4a2f2ea4.ttf"
+  }
+]);
