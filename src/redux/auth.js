f84ad78ffb4ce5d2745035134d07fb922d323487
DavidRinSE <DavidRichardsonIN@gmail.com>
2020-07-27 18:28:21 -0400

    began error handling

--- src/redux/auth.js
+++ src/redux/auth.js
@@ -11,65 +11,61 @@ import {client} from "./index"
 import {gql} from "apollo-boost"
 
 export const LOG_IN = createActions("LOG_IN")
-export const getLoginToken = (data) => dispatch => {
+export const getLoginToken = (input) => dispatch => {
     dispatch(LOG_IN.START());
-    const {username, password} = data
+    const {username, password} = input
 
     return client.mutate({
         mutation: gql`
             mutation login {
                 login(username: "${username}", password: "${password}") {
                     token
-                    error {
-                        message
-                    }
                 }
             }
         `
     }).then(res => {
-        if(res.data && res.data.login){
-            if(res.data.login.token){
-                dispatch(LOG_IN.SUCCESS(res.data.login.token))
-            } else if (res.data.login.error) {
-                dispatch(LOG_IN.FAIL(res.data.login.error.message))
-            } else {
-                dispatch(LOG_IN.FAIL("Something went wrong 😖"))
-            }
+        const {data} = res
+        if(data && data.login && data.login.token){
+            dispatch(LOG_IN.SUCCESS(res.data.login.token))
         } else {
             dispatch(LOG_IN.FAIL("Something went wrong 😖"))
+        }
+    }).catch(e => {
+        if(e.graphQLErrors) {
+            dispatch(LOG_IN.FAIL(e.graphQLErrors[0].message))
         }
     })
     // return logIn(username, password).then(res => dispatch(LOG_IN.SUCCESS(res)))
 }
 
 export const SIGN_UP = createActions("SIGN_UP")
-export const signUpUser = (data) => dispatch => {
+export const signUpUser = (input) => dispatch => {
     dispatch(SIGN_UP.START());
-    const {username, password} = data
+    const {username, password} = input
 
     return client.mutate({
         mutation: gql`
             mutation createUser {
                 createUser(username: "${username}", password: "${password}") {
                     token
-                    error {
-                        message
-                    }
                 }
             }
         `
     }).then(res => {
         if(res.data && res.data.createUser){
-            if(res.data.createUser.token){
+            const {data} = res
+            if(data.createUser.token){
                 dispatch(LOG_IN.SUCCESS(res.data.createUser.token))
                 dispatch(SIGN_UP.SUCCESS("Success!"))
-            } else if (res.data.createUser.error) {
-                dispatch(SIGN_UP.FAIL(res.data.createUser.error.message))
             } else {
                 dispatch(SIGN_UP.FAIL("Something went wrong 😖"))
             }
         } else {
             dispatch(SIGN_UP.FAIL("Something went wrong 😖"))
+        }
+    }).catch(e => {
+        if(e.graphQLErrors){
+            dispatch(SIGN_UP.FAIL(e.graphQLErrors[0].message))
         }
     })
     // return logIn(username, password).then(res => dispatch(LOG_IN.SUCCESS(res)))
